affd1477d889424a271d4eb2834319ec054c9fca
“BT <biantaobrad@gmail.com>
2018-01-30 21:41:31 +0800

    20180130

--- StringTest.java
+++ StringTest.java
@@ -1,3 +1,5 @@
+import javax.sound.midi.Soundbank;
+
 /**
  * <p>类描述：
  * <pre>
@@ -11,5 +13,17 @@
  * @since 2018年01月30日
  */
 public class StringTest {
-
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "hello";
+        String str3 = new String("hello");
+        String str4 = new String("hello");
+        String str5 = 
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str2));
+        System.out.println(str3==str4);
+        System.out.println(str3.equals(str4));
+        System.out.println(str3==str2);
+        System.out.println(str3.equals(str2));
+    }
 }
